4b6a83b4b7664dbc470afeed593af0b2c4bb31cd
Szymon Olewniczak <(My first name)@imz.re>
2016-03-22 11:23:04 +0100

    specify ireadit users

--- action.php
+++ action.php
@@ -10,27 +10,30 @@ require_once DOKU_PLUGIN.'syntax.php';
  * need to inherit from this class
  */
 class action_plugin_ireadit extends DokuWiki_Action_Plugin {
-
     function register(Doku_Event_Handler $controller) {
 		$controller->register_hook('TPL_CONTENT_DISPLAY', 'AFTER',  $this, 'add_link_and_list');
 		$controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE',  $this, 'add_to_ireadit_metadata');
 		$controller->register_hook('PARSER_METADATA_RENDER', 'AFTER',  $this, 'updatre_ireadit_metadata');
+		$controller->register_hook('PARSER_CACHE_USE','BEFORE', $this, 'handle_cache_prepare');
     }
     function has_read($readers, $who) {
-	 foreach ($readers as $reader)
-		if ($reader['name'] == $who)
+    	if (!is_array($readers))
+    		return false;
+	foreach ($readers as $reader)
+		if ($reader['client'] == $who)
 			return true;
 		return false;
     }
     function add_link_and_list($event)
     {
-		global $ID, $INFO, $ACT, $REV;
+		global $ID, $INFO, $ACT, $REV, $auth;
 
 		if($ACT != 'show')
 			return;
 		
 		if(!p_get_metadata($ID, 'plugin_ireadit_display'))
 			return;
+		
 		
 		$plugin_ireadit = p_get_metadata($ID, 'plugin_ireadit');
 		if ($REV == 0)
@@ -43,14 +46,26 @@ class action_plugin_ireadit extends DokuWiki_Action_Plugin {
 			if ($this->getConf('print') == 0)
 				echo ' class="no-print"';
 		echo '>';
-		if($REV == 0 && is_array($INFO['userinfo']) && !$this->has_read($readers, $INFO['userinfo']['name']))
+		
+		//check if the user should read the page
+		$all_users = p_get_metadata($ID, 'plugin_ireadit_all_users');
+		$users = p_get_metadata($ID, 'plugin_ireadit_users');
+		$groups = p_get_metadata($ID, 'plugin_ireadit_groups');
+		
+		//if we using old version of plugin varibles may not exist
+		if (
+		    ($all_users === TRUE) ||
+		    ($all_users === FALSE && (in_array($INFO['client'], $users) || count(array_intersect($INFO['userinfo']['grps'], $groups)) > 0))
+		) if($REV == 0 && is_array($INFO['userinfo']) && !$this->has_read($readers, $INFO['client']))
 			echo '<a href="?id='.$ID.'&do=ireadit">'.$this->getLang('ireadit').'</a>';
 		
 		if(count($readers) > 0){
 			echo '<h3>'.$this->getLang('readit_header').'</h3>';
 			echo '<ul>';
-			foreach ($readers as $reader)
-			   echo '<li>'.$reader['name'].' - '.date('d/m/Y H:i', $reader['time']).'</li>'; 
+			foreach ($readers as $reader) {
+			   $udata = $auth->getUserData($reader['client'], false);
+			   echo '<li>'.$udata['name'].' - '.date('d/m/Y H:i', $reader['time']).'</li>';
+			}
 
 			echo '</ul>';
 		}
@@ -75,6 +90,15 @@ class action_plugin_ireadit extends DokuWiki_Action_Plugin {
 		$ACT = 'show';
 		if(!is_array($INFO['userinfo']))
 			return;
+		
+		//user can ireadit the page
+		$all_users = p_get_metadata($ID, 'plugin_ireadit_all_users');
+		$users = p_get_metadata($ID, 'plugin_ireadit_users');
+		$groups = p_get_metadata($ID, 'plugin_ireadit_groups');
+		if (!(
+		    ($all_users === TRUE) ||
+		    ($all_users === FALSE && (in_array($INFO['client'], $users) || count(array_intersect($INFO['userinfo']['grps'], $groups)) > 0))
+		)) return;
 			
 		$plugin_ireadit = p_get_metadata($ID, 'plugin_ireadit');
 	   	$date = p_get_metadata($ID, 'date modified');
@@ -82,11 +106,28 @@ class action_plugin_ireadit extends DokuWiki_Action_Plugin {
 	   		$plugin_ireadit = array($date => array());
 		
 	   	$readers = $plugin_ireadit[$date];
-		$user = $INFO['userinfo']['name'];
-	    //check if user has read the page already
-	   	if (!$this->has_read($readers, $user)) {
-			$plugin_ireadit[$date][] = array('name' => $user, 'time' => time());
+		$client = $INFO['client'];
+	        //check if user has read the page already
+	        //check if user can 
+	   	if (!$this->has_read($readers, $client)) {
+			$plugin_ireadit[$date][] = array('client' => $client, 'time' => time());
 			p_set_metadata($ID, array('plugin_ireadit' => $plugin_ireadit));
 		}
 	}
+
+    /**
+     * prepare the cache object for default _useCache action
+     */
+    function handle_cache_prepare(&$event, $param) {
+        $cache =& $event->data;
+        
+        // we're only interested in wiki pages
+        if (!isset($cache->page)) return;
+        if ($cache->mode != 'i') return;
+        
+        // get meta data
+        $ireadit  = p_get_metadata($cache->page, 'plugin_ireadit_display');
+        if ($ireadit)
+        	$cache->depends['purge'] = true;
+    }
 }
--- syntax.php
+++ syntax.php
@@ -18,14 +18,32 @@ class syntax_plugin_ireadit extends DokuWiki_Syntax_Plugin {
     function getType() { return 'substition'; }
     function getSort() { return 99; }
 
-
+    private $users = array(), $groups = array(), $all_users = false;
     function connectTo($mode) {
-	$this->Lexer->addSpecialPattern('~~IREADIT~~',$mode,'plugin_ireadit');
+	$this->Lexer->addEntryPattern('~~IREADIT', 'base', 'plugin_ireadit');
+	 
+	// Match user or group
+	$this->Lexer->addPattern('[^ ~]+', 'plugin_ireadit');
+	 
+	$this->Lexer->addExitPattern('~~', 'plugin_ireadit');
     }
 
     function handle($match, $state, $pos, Doku_Handler $handler)
     {
-	return true;
+        switch ( $state ) {
+            case DOKU_LEXER_MATCHED:
+                if ($match[0] == '@') {
+                	$group = substr($match, 1);
+                	$this->groups[] = $group;
+		} else 
+			$this->users[] = $match;
+            break;
+            case DOKU_LEXER_EXIT:
+            	if (count($this->users) == 0 && count($this->groups) == 0)
+            		$this->all_users = true;
+            break;
+        }
+        return TRUE;
     }
 
     function render($mode, Doku_Renderer $renderer, $data) {
@@ -33,6 +51,9 @@ class syntax_plugin_ireadit extends DokuWiki_Syntax_Plugin {
 	    return true;
 	elseif($mode == 'metadata'){
 	    $renderer->meta['plugin_ireadit_display'] = true;
+	    $renderer->meta['plugin_ireadit_users'] = $this->users;
+	    $renderer->meta['plugin_ireadit_groups'] = $this->groups;
+	    $renderer->meta['plugin_ireadit_all_users'] = $this->all_users;
 	    return true;
         }
         return false;
