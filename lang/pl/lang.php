89d6761d4711fcd3059a848b9aa5f92fa540846e
Szymon Olewniczak <solewniczak@rid.pl>
2019-07-23 09:51:56 +0200

    fix lang error

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -3,4 +3,4 @@
 $lang['ireadit'] = 'Zapoznałem się z treścią strony.';
 $lang['readit_header'] = 'Następujący użytkownicy zapoznali się z tą stroną:';
 
-$lang['notification full'] = 'Masz stronę od przeczytania: %s';
+$lang['notification full'] = 'Masz stronę do przeczytania: %s';
