6752733eff031db26f636722d8d9b0f4fadf4280
Szymon Olewniczak <solewniczak@rid.pl>
2020-03-27 16:11:03 +0100

    adds admin option to regenerate ireadit metadata

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,3 +4,11 @@ $lang['ireadit'] = 'I\'ve read it.';
 $lang['readit_header'] = 'Following people have read this page:';
 
 $lang['notification full'] = 'You have a page to read: %s';
+
+$lang['menu'] = 'Ireadit metadata';
+
+$lang['admin h_page'] = 'Page';
+$lang['admin h_meta'] = 'Metadata';
+$lang['admin h_last_change_date'] = 'Last change date';
+$lang['admin regenerate_metadata'] = 'Regenerate metadata';
+$lang['admin success'] = 'Metadata regenerated successfully.';
